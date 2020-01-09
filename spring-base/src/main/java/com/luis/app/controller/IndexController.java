632c8698e4af7a7aeb820575167fc3a5d6134a02
anton <ramirezfonseca.la@gmail.com>
2020-01-08 23:28:20 -0600

    se agrego la vista para perfil y el modelo

--- spring-base/src/main/java/com/luis/app/controller/IndexController.java
+++ spring-base/src/main/java/com/luis/app/controller/IndexController.java
@@ -4,15 +4,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.luis.app.model.Usuario;
+
 
 @Controller
 @RequestMapping("/app")
 public class IndexController {
 
+	public static final String PATH_VIEW = "/view/";
+	
 	@GetMapping("/index")//@GetMapping({"/index","/","/home"})
 	public String msn(Model model) {
 		model.addAttribute("saludo","Saludos desde Spring Boot2");
-		return "/view/index";
+		return PATH_VIEW+"index";
 	}
 	
+	@GetMapping("/perfil")
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Jose");
+		usuario.setApellido("Pulido");
+		model.addAttribute("usuario",usuario);
+		return PATH_VIEW+"perfil";
+	}
 }
